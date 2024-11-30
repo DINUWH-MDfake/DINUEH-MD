e6fd9d3d354c2c24b14b98051c558e378b2b08db
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-11-30 11:27:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/sqbYr8C/5539.jpg",
+ALIVEA_MSG: process.env.ALIVE_MG || "> hellow❤️‍🩹, *DINUWHMD* whatsaap bot is alive now🫢",
 };
