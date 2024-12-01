1941d2d0cf489cf9f9cb8bbe5e685abf2d5d738e
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-12-01 14:22:29 +0530

    Update song+videodl.js

--- plugins/song+videodl.js
+++ plugins/song+videodl.js
@@ -46,4 +46,51 @@ await conn.sendMessage(from,{audio: {downloadUrl},mimetype:"audio/mpeg"},{quoted
   reply(`${e}`)
 }
 })
+
+//============vidro-dl============
+
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or text")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc =`
+> *🌟DINU-EH-MD VIDEO DOWNLOADER🌟*
+
+title: ${data.title}
+description: ${data.discription}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+*MADE BY DINUWH-MD & POWERD BY DINUH👻*
+`
+await conn.sendMessage(frome,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video message
+await conn.sendMessage(from,{video: {downloadUrl},mimetype:"video/mp4"},{quoted:mek})
   
+
+
+
+
+}catch(e){
+  console.log(e)
+  reply(`${e}`)
+}
+})
+  
+
