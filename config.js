b933276506902899291a33281da7caa8565d50c5
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-12-02 17:03:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/sqbYr8C/5539.jpg",
-ALIVEA_MSG: process.env.ALIVE_MG || "> hellow❤️‍🩹, *DINUWHMD* whatsaap bot is alive now🫢",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-    MODE: process.env.MODE || "public",
-};
+MONGODB
+};: process.env.MONGODB || "enter mongodb here"
