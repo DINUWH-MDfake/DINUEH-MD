02baa15875aaa6b0c20a15a014725680092b95d4
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-12-02 15:13:10 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,6 +1,7 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require("os")
+const {runtime} = require(`../lib/fonction`)
 cmd({
     pattern: "system",
     alias: ["status","system","botinfo","work","activity"],
