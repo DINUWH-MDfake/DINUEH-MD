2b0d4319345eabc2c98be116675da11000d1746c
DINUWH-MDfake <dinuwahmd@gmail.com>
2024-12-24 21:46:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "jJ9CzbYD#pEtEl8DdzzaL7gn-8nsOU1QZ8MRBxPmxtT_SVwXqHNI"
+MONGODB: process.env.MONGODB || "mongodb://mongo:aHCBTsUhzVIvadJNEBXYgzCePbwwkAhj@junction.proxy.rlwy.net:51068",
 };
